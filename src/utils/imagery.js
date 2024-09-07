36824aeb309719c1c3e2f660951da126e68b54b4
mraihanaf <mraihanaf0@gmail.com>
2024-09-07 08:19:57 +0700

    fix: fix wrong function

--- src/utils/imagery.js
+++ src/utils/imagery.js
@@ -7,7 +7,7 @@ const getImageryChannel = async function getImageryChannel(guildId) {
 const getGuildMedia = async function getGuildMedia(guildId){
     const isExist = await getImageryChannel(guildId)
     if(!isExist) return null
-    const gallery = await redis?.lrange(`imagery_gallery_${guildId}`, rangeStart, rangeEnd);
+    const gallery = await redis?.lrange(`imagery_gallery_${guildId}`);
     if(!gallery) return []
     return gallery.map(media => JSON.parse(media))
 }
